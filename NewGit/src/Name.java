c6f53944a6dcceb49a362caed43dc511a127cb29
krunalp-ezdi <krunal.p@ezdi.us>
2016-01-22 15:14:42 +0530

    Update Name.java

--- NewGit/src/Name.java
+++ NewGit/src/Name.java
@@ -4,6 +4,7 @@ public class Name {
 	public static void main(String[] args) {
 		System.out.println("Krunal patel");
 		System.out.println("Something Change");
+		System.out.println("Repositary Side");
 
 	}
 
